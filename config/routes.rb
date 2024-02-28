cfc4cbb00c713e6aa48b80716770986d0a0a6801
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2024-02-29 00:19:55 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # resources :students, only: %i[create destroy]
+  # resources :schools, only: [] do
+  #   resources :classes, only: [:index] do
+  #     resources :students, only: [:index]
+  #   end
+  # end
+
+  post '/students', to: 'students#create'
+  delete '/students/:id', to: 'students#destroy'
+  get '/schools/:id/classes/:id/students', to: 'students#index'
+  get '/schools/:id/classes/', to: 'klasses#index'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
